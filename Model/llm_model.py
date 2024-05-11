525319b3461bd073005f8ed26ae1095cf1744585
Nguyễn Minh Quang <127973111+QuangNguyen2910@users.noreply.github.com>
2024-05-11 13:56:41 +0700

    Update llm_model.py applying Unsloth Framework for faster training time and faster inference time.

--- Model/llm_model.py
+++ Model/llm_model.py
@@ -1,149 +1,124 @@
+from datasets import load_dataset, Dataset
+from unsloth import FastLanguageModel
+import torch
 from transformers import (
-    AutoConfig, AutoModelForCausalLM,
-    AutoModelForSeq2SeqLM, AutoTokenizer,
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
     BitsAndBytesConfig
 )
 from peft import (
-    LoraConfig, PeftConfig, PeftModel, get_peft_model,
-    prepare_model_for_kbit_training, TaskType
+    LoraConfig,
+    PeftConfig,
+    PeftModel,
+    get_peft_model,
+    prepare_model_for_kbit_training,
+    TaskType
 )
-import torch
-import torch.nn as nn
-import json
-import os
-import bitsandbytes as bnb
-import transformers
-import pandas as pd
-import numpy as np
-import wandb
+from trl import SFTTrainer
+from transformers import TrainingArguments
 
-class LlmModel:
+class ChatBotModel:
     """
     Class for loading Large Language Models.
 
     Args:
         model_name (str): Name of the pre-trained model.
-        model_type (str): Type of the model, either "seq2seq" or another.
-        hf_access_token (str): Hugging Face token required to access some llm pretrains that need authentication
-        quantize (bool, optional): Whether to quantize the model. Defaults to False.
+        hf_access_token (str): Hugging Face token required to access some LLM pre-trains that need authentication.
+        load_in_4bit (bool, optional): Whether to load the model in 4-bit precision. Defaults to False.
+        max_seq_length (int, optional): Maximum sequence length for the input. Defaults to 2048.
+        dtype (type, optional): Data type for model weights. Defaults to None.
+        training (bool, optional): Whether the model is used for training. Defaults to False.
 
     Attributes:
-        model_name (str): Name of the pre-trained model.
-        model_type (str): Type of the model.
-        quantize (bool): Whether the model is quantized.
-        model: Loaded model object.
+        model (Any): Loaded model object.
+        tokenizer (Any): Tokenizer model for input sequence.
     """
-    
-    def __init__(self, model_name: str, model_type: str,
-                 peft_model_path: str = None,
+
+    def __init__(self,
+                 model_name: str,
                  hf_access_token: str = None,
-                 quantize: bool = False,
+                 load_in_4bit: bool = False,
+                 max_seq_length: int = 2048,
+                 dtype: type = None,
                  training: bool = False):
-        
+
         self.model_name = model_name
-        self.model_type = model_type
         self.hf_access_token = hf_access_token
-        self.peft_model_path = peft_model_path
-        self.quantize = quantize
+        self.load_in_4bit = load_in_4bit
+        self.max_seq_length = max_seq_length
+        self.dtype = dtype
         self.training = training
 
-    def get_model(self):
+        self.model = None
+        self.tokenizer = None
+
+    def load_pretrained_model(self):
         """
         Load and return the pre-trained model.
 
         Returns:
-            model: Loaded model object.
-            tokenizer: Tokenizer model for input sequence.
+            model (Any): Loaded model object.
+            tokenizer (Any): Tokenizer model for input sequence.
         """
         try:
-            bnb_config = self.get_quantization_config() if self.quantize else None
-            
-            if self.model_type == "seq2seq":
-                model = AutoModelForSeq2SeqLM.from_pretrained(
-                    self.model_name,
-                    trust_remote_code=True,
-                    quantization_config=bnb_config,
-                    token=self.hf_access_token,
-                    device_map="auto"
-                )
-            else:
-                model = AutoModelForCausalLM.from_pretrained(
-                    self.model_name,
-                    trust_remote_code=True,
-                    quantization_config=bnb_config,
-                    token=self.hf_access_token,
-                    device_map="auto"
-                )
-                
-            tokenizer = AutoTokenizer.from_pretrained(self.model_name, token=self.hf_access_token)
-            tokenizer.pad_token = tokenizer.eos_token
-            
+            model, tokenizer = FastLanguageModel.from_pretrained(
+                model_name=self.model_name,
+                max_seq_length=self.max_seq_length,
+                dtype=self.dtype,
+                load_in_4bit=self.load_in_4bit,
+                token=self.hf_access_token,
+            )
+
+            print(f"Loaded model '{self.model_name}' from Hugging Face.")
             return model, tokenizer
         except Exception as e:
-            print(f"Error loading model: {e}")
-            
-            return None
+            raise ValueError(f"Failed to load model '{self.model_name}' from Hugging Face. Error: {e}")
 
-    def get_quantization_config(self):
+    def get_pretrained_model(self):
         """
-        Return the quantization configuration.
+        Get the loaded pretrained model and tokenizer.
 
         Returns:
-            BitsAndBytesConfig: Quantization configuration.
+            model (Any): Loaded model object.
+            tokenizer (Any): Tokenizer model for input sequence.
         """
-        
-        return BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_quant_type="nf4",
-            bnb_4bit_compute_dtype=torch.bfloat16
-        )
-        
-    def get_peft_model(self, r=16, lora_alpha=32,
-                        target_modules="all-linear",
-                        lora_dropout=0.05, bias="none"):
+        if self.model is None or self.tokenizer is None:
+            self.model, self.tokenizer = self.load_pretrained_model()
+        return self.model, self.tokenizer
+
+    def apply_peft(self, r: int = 16, lora_alpha: int = 16,
+                   target_modules: list = ["q_proj", "k_proj", "v_proj", "o_proj",
+                                           "gate_proj", "up_proj", "down_proj"],
+                   use_gradient_checkpointing: str = "unsloth",
+                   lora_dropout: float = 0.05, bias: str = "none") -> tuple:
         """
-        Return the model after apply peft (Parameter-Efficient Fine-tuning).
-        
-        - r, lora_alpham, target_modules, lora_dropout, bias: Configs for LoRA
-        
-        Because peft is only use when you need do finetune a model for specific needs so:
-        + If finetuning is required: We will prepare model for k-bit training and applying peft.
-        + Else: Just apply a pretrain peft to our model for immediate use.
-        
-        Returns:
-            peft_model: Model after applying peft.
-            tokenizer: Tokenizer model for input sequence.
-        """
-        model, tokenizer = self.get_model()
-        if self.training == True:
-            model.gradient_checkpointing_enable()
-            model = prepare_model_for_kbit_training(model)
-            lora_config = self.get_lora_config()
-            peft_model = get_peft_model(model, lora_config)
-        else:
-            peft_model = PeftModel.from_pretrained(model, self.peft_model_path)
-        
-        return peft_model,tokenizer
-        
-    def get_lora_config(self, r=16, lora_alpha=32,
-                        target_modules="all-linear",
-                        lora_dropout=0.05, bias="none"):
-        """
-        Return the lora configuration.
+        Apply Parameter-Efficient Fine-tuning (PEFT) to the model.
+
+        Args:
+            r (int): Number of clusters. Defaults to 16.
+            lora_alpha (int): Alpha value for LoRA. Defaults to 16.
+            target_modules (list): Modules to apply PEFT. Defaults to ["q_proj", "k_proj", "v_proj", "o_proj",
+                                               "gate_proj", "up_proj", "down_proj"].
+            use_gradient_checkpointing (str): Gradient checkpointing method. Defaults to "unsloth".
+            lora_dropout (float): Dropout value for LoRA. Defaults to 0.05.
+            bias (str): Bias option for LoRA. Defaults to "none".
 
         Returns:
-            LoraConfig: LoRA configuration for applying PEFT.
+            tuple: Tuple containing the modified model and tokenizer.
         """
-        
-        task_type = "SEQ_2_SEQ_LM" if self.model_type == "seq2seq" else "CAUSAL_LM"
-        lora_config = LoraConfig(
-            r=16, # Rank
-            lora_alpha=32,        #ΔW=α/r | ΔW represents the change in weights due to LoRA. α as a knob that controls how much influence the LoRA activations have on the overall model.
-            target_modules="all-linear",
-            lora_dropout=0.05,
-            bias="none",
-            task_type=task_type,
+
+        model, tokenizer = self.get_pretrained_model()
+
+        model = FastLanguageModel.get_peft_model(
+            model,
+            r=r,
+            target_modules=target_modules,
+            lora_alpha=lora_alpha,
+            lora_dropout=lora_dropout,
+            bias=bias,
+            use_gradient_checkpointing=use_gradient_checkpointing,
         )
 
-        return lora_config
+        return model, tokenizer
